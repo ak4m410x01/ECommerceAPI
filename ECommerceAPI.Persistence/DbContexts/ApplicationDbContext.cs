167cad50c0386460360e8aa66538d481fac83b43
Abdullah Kamal <abdallah10kamal@gmail.com>
2024-05-29 01:24:14 +0300

    Add Sql Server ApplicationDbContext

---
+++ ECommerceAPI.Persistence/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,41 @@
+﻿using ECommerceAPI.Domain.IdentityEntities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace ECommerceAPI.Persistence.DbContexts
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        #region Constructors
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        #endregion
+
+
+        #region On Model Creating Configuration
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            #region Identity Configuration
+
+            // Config Identity Tables Names
+            builder.Entity<IdentityUserRole<string>>().ToTable("UserRoles", "Security");
+            builder.Entity<IdentityUserClaim<string>>().ToTable("UserClaims", "Security");
+            builder.Entity<IdentityUserLogin<string>>().ToTable("UserLogins", "Security");
+            builder.Entity<IdentityRoleClaim<string>>().ToTable("RoleClaims", "Security");
+            builder.Entity<IdentityUserToken<string>>().ToTable("UserTokens", "Security");
+
+            #endregion
+
+            #region Assemblies Configuration
+
+            // Apply Entities Configurations
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            #endregion
+        }
+        #endregion
+    }
+}
---
+++ ECommerceAPI.Persistence/EntityConfiguration/Users/ApplicationUserConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using ECommerceAPI.Domain.IdentityEntities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ECommerceAPI.Persistence.EntityConfiguration.Users
+{
+    public class ApplicationUserConfiguration : IEntityTypeConfiguration<ApplicationUser>
+    {
+        public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+        {
+            #region Config Table Name
+            // Config Table Name for ApplicationUser Entity
+            builder.ToTable("Users", "Users");
+            #endregion
+        }
+    }
+}
