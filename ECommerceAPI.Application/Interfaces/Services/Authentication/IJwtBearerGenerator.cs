1c66245aff7a8d487956af26ae139640c4b54e15
Abdullah Kamal <abdallah10kamal@gmail.com>
2024-05-30 18:07:25 +0300

    Add Jwt Bearer Token Generator Contract

---
+++ ECommerceAPI.Application/Interfaces/Services/Authentication/IJwtBearerGenerator.cs
@@ -0,0 +1,9 @@
+﻿using ECommerceAPI.Domain.IdentityEntities;
+
+namespace ECommerceAPI.Application.Interfaces.Services.Authentication
+{
+    public interface IJwtBearerGenerator
+    {
+        string GenerateToken(ApplicationUser user);
+    }
+}
