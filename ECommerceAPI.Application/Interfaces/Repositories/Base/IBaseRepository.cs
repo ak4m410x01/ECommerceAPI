a872cb513f48b0ca80e000ba50ace7ccde291e06
Abdullah Kamal <abdallah10kamal@gmail.com>
2024-06-02 02:00:29 +0300

    Add Generic Repository.

---
+++ ECommerceAPI.Application/Interfaces/Repositories/Base/IBaseRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace ECommerceAPI.Application.Interfaces.Repositories.Base
+{
+    public interface IBaseRepository<T> where T : class
+    {
+        Task<IQueryable<T>> GetAllAsync();
+
+        Task<T?> GetById<TId>(TId id);
+    }
+}
---
+++ ECommerceAPI.Persistence/Repositories/Base/BaseRepository.cs
@@ -0,0 +1,38 @@
+﻿using ECommerceAPI.Application.Interfaces.Repositories.Base;
+using ECommerceAPI.Persistence.DbContexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerceAPI.Persistence.Repositories.Base
+{
+    public class BaseRepository<T> : IBaseRepository<T> where T : class
+    {
+        #region Properties
+
+        private readonly ApplicationDbContext _context;
+
+        #endregion Properties
+
+        #region Constructors
+
+        public BaseRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        #endregion Constructors
+
+        #region Method
+
+        public async Task<IQueryable<T>> GetAllAsync()
+        {
+            return await Task.FromResult(_context.Set<T>().AsNoTracking());
+        }
+
+        public async Task<T?> GetById<TId>(TId id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        #endregion Method
+    }
+}
