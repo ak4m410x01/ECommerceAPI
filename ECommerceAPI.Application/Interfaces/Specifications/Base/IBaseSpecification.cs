5bca3dfdd58e18344fa422420ac302e807b1ec61
Abdullah Kamal <abdallah10kamal@gmail.com>
2024-06-02 14:16:39 +0300

    Add Contract of Specifications.

---
+++ ECommerceAPI.Application/Interfaces/Specifications/Base/IBaseSpecification.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq.Expressions;
+
+namespace ECommerceAPI.Application.Interfaces.Specifications.Base
+{
+    public interface IBaseSpecification<T> where T : class
+    {
+        Expression<Func<T, bool>> Criteria { get; set; }
+        List<Expression<Func<T, object>>> Includes { get; set; }
+    }
+}
---
+++ ECommerceAPI.Persistence/Specifications/Base/BaseSpecification.cs
@@ -0,0 +1,11 @@
+﻿using ECommerceAPI.Application.Interfaces.Specifications.Base;
+using System.Linq.Expressions;
+
+namespace ECommerceAPI.Persistence.Specifications.Base
+{
+    public class BaseSpecification<T> : IBaseSpecification<T> where T : class
+    {
+        public Expression<Func<T, bool>> Criteria { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public List<Expression<Func<T, object>>> Includes { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
