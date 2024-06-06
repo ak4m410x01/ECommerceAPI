a441b6d6835478ba008a4fef5186db59b8286e59
Abdullah Kamal <abdallah10kamal@gmail.com>
2024-06-06 03:49:06 +0300

    Add Error Handler MiddleWare.

---
+++ ECommerceAPI.Presentation/Extensions/Middlewares/Exceptions/ErrorHandlerApplicationBuilder.cs
@@ -0,0 +1,13 @@
+﻿using ECommerceAPI.Presentation.MiddleWares;
+
+namespace ECommerceAPI.Presentation.Extensions.MiddleWares.Exceptions
+{
+    public static class ErrorHandlerApplicationBuilder
+    {
+        public static IApplicationBuilder UseErrorHandlerMiddleWare(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<ErrorHandlerMiddleWare>();
+            return app;
+        }
+    }
+}
---
+++ ECommerceAPI.Presentation/MiddleWares/ErrorHandlerMiddleWare.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Net;
+using System.Text.Json;
+using FluentValidation;
+using ECommerceAPI.Shared.Responses;
+
+namespace ECommerceAPI.Presentation.MiddleWares
+{
+    public class ErrorHandlerMiddleWare
+    {
+        #region Properties
+
+        private readonly RequestDelegate _next;
+
+        #endregion Properties
+
+        #region Constructors
+
+        public ErrorHandlerMiddleWare(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        #endregion Constructors
+
+        #region Methods
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception error)
+            {
+                var response = context.Response;
+                response.ContentType = "application/json";
+                var responseModel = new Response<string>() { Succeeded = false, Message = error?.Message };
+                //TODO:: cover all validation errors
+                switch (error)
+                {
+                    case UnauthorizedAccessException e:
+                        // custom application error
+                        responseModel.Message = error.Message;
+                        responseModel.StatusCode = HttpStatusCode.Unauthorized;
+                        response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                        break;
+
+                    case ValidationException e:
+                        // custom validation error
+                        responseModel.Message = error.Message;
+                        responseModel.StatusCode = HttpStatusCode.UnprocessableEntity;
+                        response.StatusCode = (int)HttpStatusCode.UnprocessableEntity;
+                        break;
+
+                    case KeyNotFoundException e:
+                        // not found error
+                        responseModel.Message = error.Message; ;
+                        responseModel.StatusCode = HttpStatusCode.NotFound;
+                        response.StatusCode = (int)HttpStatusCode.NotFound;
+                        break;
+
+                    case DbUpdateException e:
+                        // can't update error
+                        responseModel.Message = e.Message;
+                        responseModel.StatusCode = HttpStatusCode.BadRequest;
+                        response.StatusCode = (int)HttpStatusCode.BadRequest;
+                        break;
+
+                    case Exception e:
+                        if (e.GetType().ToString() == "ApiException")
+                        {
+                            responseModel.Message += e.Message;
+                            responseModel.Message += e.InnerException == null ? "" : "\n" + e.InnerException.Message;
+                            responseModel.StatusCode = HttpStatusCode.BadRequest;
+                            response.StatusCode = (int)HttpStatusCode.BadRequest;
+                        }
+                        responseModel.Message = e.Message;
+                        responseModel.Message += e.InnerException == null ? "" : "\n" + e.InnerException.Message;
+
+                        responseModel.StatusCode = HttpStatusCode.InternalServerError;
+                        response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                        break;
+
+                    default:
+                        // unhandled error
+                        responseModel.Message = error?.Message;
+                        responseModel.StatusCode = HttpStatusCode.InternalServerError;
+                        response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                        break;
+                }
+
+                await response.WriteAsJsonAsync(responseModel);
+            }
+        }
+
+        #endregion Methods
+    }
+}
