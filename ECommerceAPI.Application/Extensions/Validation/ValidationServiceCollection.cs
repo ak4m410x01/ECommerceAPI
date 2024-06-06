1435e39a4e20863269b88f2b320ceb63df79ffa2
Abdullah Kamal <abdallah10kamal@gmail.com>
2024-06-06 03:48:22 +0300

    Add Validation Service Collection.

---
+++ ECommerceAPI.Application/Extensions/Validation/ValidationServiceCollection.cs
@@ -0,0 +1,18 @@
+﻿using ECommerceAPI.Application.Behaviors.Validations;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace ECommerceAPI.Application.Extensions.Validation
+{
+    public static class ValidationServiceCollection
+    {
+        public static IServiceCollection AddValidationConfiguration(this IServiceCollection services)
+        {
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddScoped(typeof(IPipelineBehavior<,>), typeof(ValidationPipelineBehavior<,>));
+            return services;
+        }
+    }
+}
